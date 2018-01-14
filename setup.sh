61f4619838cbdf7fa5254b65b2ee4f1c1ce650bf
maumau <maumaumaumaumaumaumaumaumaumau@gmail.com>
2018-01-14 20:40:22 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-cp ~/.bashrc ~/.bashrc.emacs2vim.back
-cp ./.rand_emacs.sh ~/
-chmod 755 ~/.rand_emacs.sh
+cp ~/.bashrc ~/.bashrc.not_emacs_rand_original.back
 
 # please use ">>" not ">"
-echo ""                         >> ~/.bashrc
-echo "function emacs() {"       >> ~/.bashrc
-echo "    ~/.rand_emacs.sh \$@" >> ~/.bashrc
-echo "}"                        >> ~/.bashrc
+echo ""                                  >> ~/.bashrc
+echo "function emacs() {"                >> ~/.bashrc
+echo "    if [ $(($RANDOM%10)) -lt 2 ];" >> ~/.bashrc
+echo "    then"                          >> ~/.bashrc
+echo "        vim $@"                    >> ~/.bashrc
+echo "    else"                          >> ~/.bashrc
+echo "        emacs $@"                  >> ~/.bashrc
+echo "    fi $@"                         >> ~/.bashrc
+echo "}"                                 >> ~/.bashrc
 
 source ~/.bashrc
